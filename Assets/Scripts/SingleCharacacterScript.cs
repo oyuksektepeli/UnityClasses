7ef1cfec473f88ac1feda525777bf9e73633d899
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-11-14 20:09:20 +0300

    Initial commit

---
+++ Assets/Scripts/SingleCharacacterScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class SingleCharacacterScript : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+    }
+
+    public Stuff mystuff = new Stuff(10, 7, 5);
+    public float speed;
+    public float turnspeed;
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shoot();
+    }
+
+    void Movement()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && mystuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            mystuff.bullets--;
+        }
+    }
+}
